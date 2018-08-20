1fd5b1402431babe50486493f31f210c0b95ec94
Ton That Cu Ly <tonly01011990@gmail.com>
2018-08-20 11:59:57 +0700

    fix edit user

--- src/components/course/UserList.js
+++ src/components/course/UserList.js
@@ -6,7 +6,7 @@ class UserList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            item: {},
+            userEdit: [],
             user: {
                 username: '',
                 password: ''
@@ -19,6 +19,7 @@ class UserList extends React.Component {
         this.getUsers();
     }
 
+    // get list user from database
     getUsers = _ => {
         fetch('http://localhost:8080/users')
             .then(response => response.json())
@@ -26,6 +27,7 @@ class UserList extends React.Component {
             .catch(err => console.error(err))
     }
 
+    // Add user into database
     addUser = _ => {
         const { user } = this.state;
         if(user.username == '' || user.password == ''){
@@ -38,6 +40,7 @@ class UserList extends React.Component {
         
     }
 
+    //delete user from database
     deleteUser (item) {
         const id = item.id;
         fetch(`http://localhost:8080/users/deleteUser?id=${id}`)  
@@ -46,16 +49,35 @@ class UserList extends React.Component {
             )
     }
 
-    editUser (item) {
-        const id = item.id;     
-        this.props.history.replace(`/menu/EditUser/:${id}`,{item: this.state.item})        
+    //call Modal Edit user
+    replaceModalItem (item) {
+        console.log(item);       
+       this.setState({
+        userEdit: item
+       }) 
+                 
+    }
+
+    // save user after edit
+    saveUser() {
+        const { userEdit } = this.state; 
+        console.log(userEdit);          
+        if(userEdit.username == '' || userEdit.password == ''){
+            alert('username and password are required !');
+          }else{
+            fetch(`http://localhost:8080/users/editUser?username=${userEdit.username}&password=${userEdit.password}&id=${userEdit.id}`)
+              .then(this.getUsers)           
+              .catch(err => console.error(err))              
+          }
+          this.props.history.push('/menu/userList');
     }
 
     render() {
-        const { users, user } = this.state;
+        const { users, user, userEdit } = this.state;       
         return (
             <div className="App">
             <br/>
+            {/* Add user form */}
                  <div>
                     <div className="form-inline">
                         <input type="text" className="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInput" placeholder="user name"
@@ -72,6 +94,7 @@ class UserList extends React.Component {
                     </div>
                 </div>
                 <br/>
+                {/* Table User List*/}
                 <div >
                     <table className="table table-active table-hover">
                         <thead>
@@ -90,22 +113,47 @@ class UserList extends React.Component {
                                         <td>{item.username}</td>
                                         <td>{item.password}</td>
                                         <td>
-                                            <button className="btn btn-primary " onClick={() => this.editUser(item)}>
+                                            <button className="btn btn-primary" data-toggle="modal" data-target="#myModal"
+                                             onClick={() => this.replaceModalItem(item)}>
                                                 <span className="fa fa-pencil fa-fw"></span>
                                             </button> &nbsp;
                                             <button className="btn btn-danger" onClick={()=> this.deleteUser(item)}>
                                                 <span className="fa fa-trash-o fa-fw"></span>
                                             </button>
-                                        </td>
-                                       
-
+                                        </td>                                       
                                     </tr>
                                 )
                             }
                         </tbody>
                     </table>
-                </div>
-               
+                </div>  
+                {/* Modal edit User */}
+                <div className="modal fade" id="myModal">
+                    <div className="modal-dialog">
+                        <div className="modal-content"> 
+                            <div className="modal-header">
+                                <h4 className="modal-title">Edit User</h4>
+                                <button type="button" className="close" data-dismiss="modal">&times;</button>
+                            </div>                                               
+                            <div className="modal-body">
+                                <div className="form-inline">
+                                    <input type="text" className="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInput" defaultValue={userEdit.username}
+                                        onChange={e => this.setState({ userEdit: { ...userEdit, username: e.target.value } })}
+                                        required
+                                    />
+                                    <input type="text" className="form-control" id="inlineFormInputGroup" defaultValue={userEdit.password}
+                                        onChange={e => this.setState({ userEdit: { ...userEdit, password: e.target.value } })}
+                                        required
+                                    /> 
+                                </div>
+                            </div>                      
+                            <div className="modal-footer">
+                              <button type="button" className="btn btn-primary" data-dismiss="modal" onClick={() => this.saveUser()}>Save</button>
+                              <button type="button" className="btn btn-danger" data-dismiss="modal">Close</button>
+                            </div>                           
+                        </div>
+                    </div>
+                </div>             
             </div>
         );
     }
